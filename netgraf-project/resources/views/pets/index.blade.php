7ac3596c30dede226fa4cd59055a3176d43268dc
Michał Janik <mjanik45@gmail.com>
2024-05-15 16:46:17 +0200

    Make status dynamic and fix Api empty key

--- netgraf-project/resources/views/pets/index.blade.php
+++ netgraf-project/resources/views/pets/index.blade.php
@@ -5,15 +5,19 @@
     <form action="{{ route('pets.index') }}" method="GET">
         <label for="status">Filter by Status:</label>
         <select name="status" id="status">
-            <option value="available" {{ request('status') === 'available' ? 'selected' : '' }}>Available</option>
-            <option value="pending" {{ request('status') === 'pending' ? 'selected' : '' }}>Pending</option>
-            <option value="sold" {{ request('status') === 'sold' ? 'selected' : '' }}>Sold</option>
+            @foreach ($statuses as $status)
+                <option value="{{ $status }}" {{ $status == request('status') ? 'selected' : '' }}>{{ ucfirst($status) }}</option>
+            @endforeach
         </select>
         <button type="submit">Filter</button>
     </form>
     <a href="{{ route('pets.create') }}">Add New Pet</a>
     <ul>
         @foreach ($pets as $pet)
+            @if (!isset($pet['name']))
+                @continue
+            @endif
+
             <li>
                 {{ $pet['name'] }}
                 <a href="{{ route('pets.edit', $pet['id']) }}">Edit</a>
